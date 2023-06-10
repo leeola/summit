d163696fab713ae91a109af1735c8b06312f14d4
Lee Olayvar <leegit@fastmail.com>
2023-06-10 11:35:39 -0700

    Initial commit

---
+++ summit/src/bin/summit.rs
@@ -0,0 +1,19 @@
+use tracing::{info, metadata::LevelFilter, subscriber};
+use tracing_subscriber::EnvFilter;
+
+fn main() {
+    // TODO: Move logging init to a core utility, for ease of test setup.
+    subscriber::set_global_default(
+        tracing_subscriber::FmtSubscriber::builder()
+            .with_env_filter(
+                EnvFilter::builder()
+                    .with_default_directive(LevelFilter::INFO.into())
+                    .with_env_var("RUST_LOG")
+                    .from_env_lossy(),
+            )
+            .finish(),
+    )
+    .unwrap();
+
+    info!("init")
+}
